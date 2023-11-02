5f8f2b9e5c1d73e87e54da1301c1eb06c5c8753d
Chaewon Yoon <ycw0363@gmail.com>
2023-11-02 17:37:51 +0900

    feat: PhoneViewController 조건에 따라 buttonEnabled RxSwift로 구현

--- SeSACRxThreads/SignUpOptional/PhoneViewController.swift
+++ SeSACRxThreads/SignUpOptional/PhoneViewController.swift
@@ -7,11 +7,25 @@
  
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
+
+// TODO: - 1101
+/// 1. button color, textfield color, textfield border color를 우선 red로, buttonEnabled도 우선 false, textfield에 기본 010 추가하기
+/// 2. 사용자가 textfield작성 -> formatted해서 데이터로 넘기기
+/// 3. text가 10자 이상이면 color blue, button enabled상태로 바꾸기
+// TODO: -
 
 class PhoneViewController: UIViewController {
    
     let phoneTextField = SignTextField(placeholderText: "연락처를 입력해주세요")
     let nextButton = PointButton(title: "다음")
+    
+    let phoneText = BehaviorSubject(value: "010")
+    let enabledColor = BehaviorSubject(value: UIColor.red)
+    let buttonEnabled = BehaviorSubject(value: false)
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +35,46 @@ class PhoneViewController: UIViewController {
         configureLayout()
         
         nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        
+        bind()
+    }
+    
+    func bind() {
+        phoneText
+            .bind(to: phoneTextField.rx.text)
+            .disposed(by: disposeBag)
+        
+        enabledColor
+            .bind(to: nextButton.rx.backgroundColor,
+                  phoneTextField.rx.tintColor)
+            .disposed(by: disposeBag)
+        
+        enabledColor
+            .map { $0.cgColor }
+            .bind(to: phoneTextField.layer.rx.borderColor)
+            .disposed(by: disposeBag)
+        
+        buttonEnabled
+            .bind(to: nextButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        //---
+        
+        phoneTextField.rx.text.orEmpty
+            .subscribe(with: self) { owner, value in
+                let result = value.formated(by: "###-####-####")
+                owner.phoneText.onNext(result)
+            }
+            .disposed(by: disposeBag)
+        //---
+        
+        phoneText
+            .map { $0.count > 10 }
+            .subscribe(with: self) { owner, value in
+                let color = value ? UIColor.blue : UIColor.red
+                owner.enabledColor.onNext(color)
+                owner.buttonEnabled.onNext(value)
+            }
+            .disposed(by: disposeBag)
     }
     
     @objc func nextButtonClicked() {
